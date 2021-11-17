a9ac96949da36e3d92b5abc78f0f00a71d50221c
Sonael <sonaelneto@gmail.com>
2021-11-17 19:56:05 -0300

    Criacao do Projeto

---
+++ src/test/java/com/api/engsoftwaremodulo2/EngSoftwareModulo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.engsoftwaremodulo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngSoftwareModulo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
