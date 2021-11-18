a2d97b9cc01af2fefffc01089880adc906329327
IlfGauhnith <lucasbburle@gmail.com>
2021-11-18 20:07:14 -0300

    ValorCondominioInvalidoException

---
+++ src/main/java/com/api/engsoftwaremodulo2/exception/ValorCondominioInvalidoException.java
@@ -0,0 +1,7 @@
+package com.api.engsoftwaremodulo2.exception;
+
+public class ValorCondominioInvalidoException extends RuntimeException {
+    public ValorCondominioInvalidoException(String message) {
+        super(message);
+    }
+}
