34684b725bfffffe55d23bc40fe4b935ee083900
IlfGauhnith <lucasbburle@gmail.com>
2021-11-18 19:01:28 -0300

    CepInvalidoException

---
+++ src/main/java/com/api/engsoftwaremodulo2/exception/CepInvalidoException.java
@@ -0,0 +1,10 @@
+package com.api.engsoftwaremodulo2.exception;
+
+
+public class CepInvalidoException extends RuntimeException {
+
+    public CepInvalidoException(String message) {
+        super(message);
+    }
+
+}
