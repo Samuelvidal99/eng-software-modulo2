708caba31935679eb12f0ab40dea2e178b4a172b
IlfGauhnith <lucasbburle@gmail.com>
2021-11-20 18:21:58 -0300

    Model imovel atualizado

--- src/main/java/com/api/engsoftwaremodulo2/model/Imovel.java
+++ src/main/java/com/api/engsoftwaremodulo2/model/Imovel.java
@@ -1,10 +1,17 @@
 package com.api.engsoftwaremodulo2.model;
 
+import com.api.engsoftwaremodulo2.deserializer.CepDeserializer;
 import com.api.engsoftwaremodulo2.exception.CepInvalidoException;
 import com.api.engsoftwaremodulo2.exception.ValorCondominioInvalidoException;
+import com.api.engsoftwaremodulo2.validator.CepNumberConstraint;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.PositiveOrZero;
 
 @Document
 @Data
@@ -12,10 +19,17 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Imovel {
 
     @Id
+    @Null
     private String id;
+    @NotBlank(message = "O campo <cidade> é obrigatório")
     private String cidade;
+    @NotBlank(message = "O campo <logradouro> é obrigatório")
     private String logradouro;
+    @NotBlank(message = "O campo <cep> é obrigatório")
+    @CepNumberConstraint
+    @JsonDeserialize(using = CepDeserializer.class)
     private String cep;
+    @PositiveOrZero(message = "O campo <valorCondominio> deve ser maior ou igual a zero")
     private double valorCondominio;
 
     public Imovel() { }
@@ -23,8 +37,8 @@ public class Imovel {
     public Imovel(String cidade, String logradouro, String cep, double valorCondominio) {
         this.cidade = cidade;
         this.logradouro = logradouro;
-        this.setCep(cep);
-        this.setValorCondominio(valorCondominio);
+        this.cep = cep;
+        this.valorCondominio = valorCondominio;
     }
 
 
@@ -61,38 +75,11 @@ public class Imovel {
 		return valorCondominio;
 	}
 
-	/**
-     * Método checa se o cep é válido. Um cep válido contém 8 números e nada mais.
-     * @param cep Uma string contendo o cep a ser validado.
-     * @return true(válido), false(inválido)
-     */
-    public boolean validarCep(String cep) {
-        return cep.length() == 8 && cep.matches("^\\d+$");
-    }
-
-    /**
-     * Método formata um dado cep removendo os caractéres comuns de separação entre os dígitos.
-     * @param cep Uma string contendo o cep a ser formatado.
-     * @return cepFormatado
-     */
-    @SuppressWarnings("RegExpRedundantEscape")
-    public String formatarCep(String cep) {
-        return cep.replaceAll("[-\\.]", "");
-    }
-
     public void setCep(String cep) {
-        cep = this.formatarCep(cep);
-
-        if (!this.validarCep(cep))
-            throw new CepInvalidoException("Não é possível atribuir um cep inválido a um imóvel.\nCEP: " + this.cep);
-
         this.cep = cep;
     }
 
     public void setValorCondominio(double valorCondominio) {
-        if (valorCondominio <= 0)
-            throw new ValorCondominioInvalidoException("Valor de condomínio inválido.\nVALOR: " + this.valorCondominio);
-
         this.valorCondominio = valorCondominio;
     }
 }
