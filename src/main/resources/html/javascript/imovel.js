c68d8999e53e974efb96ac89216d01a97fbe32eb
Samuelvidal99 <samuelvidal2014@hotmail.com>
2021-11-20 16:41:06 -0300

    Mudanças na tela alterar imóvel

---
+++ src/main/resources/html/javascript/imovel.js
@@ -0,0 +1,26 @@
+
+function teste() {
+    var imovel = document.getElementById("contratos").value
+
+    cidade = document.getElementById("cidadeField")
+    logradouro = document.getElementById("logradouroField")
+    cep = document.getElementById("cepField")
+    condominio = document.getElementById("condominioField")
+
+    if(imovel == "Imóvel 01") {
+        cidade.value = "Cidade Imóvel 01"
+        logradouro.value = "Logradouro Imóvel 01"
+        cep.value = "CEP Imóvel 01"
+        condominio.value = "Condomínio Imóvel 01"
+    } else if (imovel == "Imóvel 02") {
+        cidade.value = "Cidade Imóvel 02"
+        logradouro.value = "Logradouro Imóvel 02"
+        cep.value = "CEP Imóvel 02"
+        condominio.value = "Condomínio Imóvel 02"
+    } else {
+        cidade.value = "Cidade Imóvel 03"
+        logradouro.value = "Logradouro Imóvel 03"
+        cep.value = "CEP Imóvel 03"
+        condominio.value = "Condomínio Imóvel 03"
+    }
+}
