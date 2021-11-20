271a33ccd4f2dea18b222c89bb402e7998ff6a30
IlfGauhnith <lucasbburle@gmail.com>
2021-11-20 13:55:36 -0300

    Novas funcionalidades implementadas em ImovelService

--- src/main/java/com/api/engsoftwaremodulo2/service/ImovelService.java
+++ src/main/java/com/api/engsoftwaremodulo2/service/ImovelService.java
@@ -2,8 +2,17 @@ package com.api.engsoftwaremodulo2.service;
 
 import com.api.engsoftwaremodulo2.model.Imovel;
 
+import java.util.List;
+
 public interface ImovelService {
 
     Imovel criar(Imovel imovel);
 
+    List<Imovel> obterTodos();
+
+    Imovel obterPorId(String id);
+
+    void deletar(String id);
+
+    Imovel atualizar(Imovel imovel);
 }
