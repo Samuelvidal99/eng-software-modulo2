5efd7e673bc8fb518bca0eeae474489b0cb264de
IlfGauhnith <lucasbburle@gmail.com>
2021-11-24 18:19:11 -0300

    Endpoints de get, put e delete implementados

--- src/main/java/com/api/engsoftwaremodulo2/controller/ImovelController.java
+++ src/main/java/com/api/engsoftwaremodulo2/controller/ImovelController.java
@@ -1,14 +1,18 @@
 package com.api.engsoftwaremodulo2.controller;
 
-import com.api.engsoftwaremodulo2.exception.CepInvalidoException;
 import com.api.engsoftwaremodulo2.exception.ConstraintException;
 import com.api.engsoftwaremodulo2.model.Imovel;
 import com.api.engsoftwaremodulo2.service.ImovelService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.Collection;
+import java.util.List;
 
 
 @RestController
@@ -18,13 +22,45 @@ public class ImovelController {
     @Autowired
     private ImovelService imovelService;
 
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Imovel> obterPorCodigo(@PathVariable
+                                                 @NotBlank(message = "codigo nao pode ser vazio") String codigo) {
+
+        Imovel imovel = this.imovelService.obterPorId(codigo);
+        return ResponseEntity.ok(imovel);
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Imovel>> obterTodos() {
+        List<Imovel> imoveis = this.imovelService.obterTodos();
+        return ResponseEntity.ok(imoveis);
+    }
+
     @PostMapping("/criar")
     public ResponseEntity<Imovel> criar(@Valid @RequestBody Imovel imovel, BindingResult bindingResult) {
-
-        if (bindingResult.hasErrors())
-            throw new ConstraintException(bindingResult.getAllErrors().get(0).getDefaultMessage());
+        this.checkBindingResultForErrors(bindingResult);
 
         Imovel novoImovel = this.imovelService.criar(imovel);
         return ResponseEntity.ok().body(novoImovel);
+    }
+
+    @PutMapping("/alterar")
+    public ResponseEntity<Imovel> alterar(@Valid @RequestBody Imovel imovel, BindingResult bindingResult) {
+        this.checkBindingResultForErrors(bindingResult);
+
+        Imovel updatedImovel = this.imovelService.atualizar(imovel);
+        return ResponseEntity.ok(updatedImovel);
+    }
+
+    @DeleteMapping("deletar/{codigo}")
+    public ResponseEntity<?> deletar(@PathVariable
+                                      @NotBlank(message = "codigo nao pode ser vazio") String codigo) {
+        this.imovelService.deletar(codigo);
+        return ResponseEntity.noContent().build();
+    }
+
+    private void checkBindingResultForErrors(BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            throw new ConstraintException(bindingResult.getAllErrors().get(0).getDefaultMessage());
     }
 }
