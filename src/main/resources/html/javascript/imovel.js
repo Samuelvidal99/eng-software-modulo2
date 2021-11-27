9f01118db4b5625d33a88ef6f48f2f05b13b30b8
Samuelvidal99 <samuelvidal2014@hotmail.com>
2021-11-27 15:30:44 -0300

    Javascript da tela cadastrar imóvel implementado

--- src/main/resources/html/javascript/imovel.js
+++ src/main/resources/html/javascript/imovel.js
@@ -1,4 +1,17 @@
 
+function alertaError(message) {
+    var alerta = `
+    <div class="alert alert-warning alert-dismissible fade show" id="alerta" role="alert">
+      <strong>${message}</strong>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    `
+    div = document.getElementById("div01")
+    $(div).append(alerta)
+}
+
 function preencherCampos() {
     var imovel = document.getElementById("contratos").value
 
@@ -23,4 +36,38 @@ function preencherCampos() {
         cep.value = "CEP Imóvel 03"
         condominio.value = "Condomínio Imóvel 03"
     }
+}
+
+function cadastrarImovel() {
+    var cidade = document.getElementById("cidadeField").value
+    var logradouro = document.getElementById("logradouroField").value
+    var cep = document.getElementById("cepField").value
+    var condominio = document.getElementById("condominioField").value
+
+    var obj = new Object()
+    obj.cidade = cidade
+    obj.logradouro = logradouro
+    obj.cep = cep
+    obj.valorCondominio = parseFloat(condominio) 
+
+    var body = JSON.stringify(obj)
+
+    console.log(body)
+
+    fetch("http://localhost:8080/imovel/new", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: body,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.message == undefined) {
+            console.log("Success: \n", data)
+        } else {
+            console.log(data.message)
+            alertaError(data.message)
+        }
+    })
 }
