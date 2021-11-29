3f59f6d22792f81782ec3faf0f3fde0888235d94
Samuelvidal99 <samuelvidal2014@hotmail.com>
2021-11-29 11:12:19 -0300

    JS das tela alterar e cadastrar email finalizado

--- src/main/resources/html/javascript/email.js
+++ src/main/resources/html/javascript/email.js
@@ -13,6 +13,20 @@ function alertaError(message) {
   window.scrollTo(0, 0)
 }
 
+function alertaSuccess(message) {
+  var alerta = `
+  <div class="alert alert-success alert-dismissible fade show" id="alerta" role="alert">
+    <strong>${message}</strong>
+    <button type="button" class="close" data-dismiss="alert" onfocus aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  `
+  div = document.getElementById("div01")
+  $(div).append(alerta)
+  window.scrollTo(0, 0)
+}
+
 function enviarEmail(){
 
   const email = document.getElementById("cadastrarEmailField")
@@ -34,27 +48,50 @@ function enviarEmail(){
 
 }
 
-function preencherSelectContratos() {
-  var select = document.getElementById("contratos")
-  fetch("http://localhost:8080/contrato/", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    if(data.length == 0) {
-      var option = "<option>Não existem Contratos cadastrados no sistema.</option>"
-      $(select).append(option)
-    }else {
-      var aux = "<option>Selecione um Contrato</option>"
-      $(select).append(aux)
-      for(var i = 0; i < data.length; i++) {
-        var option = `<option value="${data[i]["id"]}">Contrato ${data[i]["num_contrato"]}</option>`
-        $(select).append(option)
+// function preencherSelectContratos() {
+//   var select = document.getElementById("contratos")
+//   fetch("http://localhost:8080/contrato/", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     if(data.length == 0) {
+//       var option = "<option>Não existem Contratos cadastrados no sistema.</option>"
+//       $(select).append(option)
+//     }else {
+//       var aux = "<option>Selecione um Contrato</option>"
+//       $(select).append(aux)
+//       for(var i = 0; i < data.length; i++) {
+//         var option = `<option value="${data[i]["id"]}">Contrato ${data[i]["num_contrato"]}</option>`
+//         $(select).append(option)
+//       }
+//     }
+//   })
+// }
+
+function cadastrarEmail() {
+  var email = document.getElementById("cadastrarEmailField").value
+  if(email != "") {
+    var url = `http://localhost:8080/conf/email/cadastrar?email=${email}`
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      if(!response.ok){
+        alertaError("E-mail Inválido")
+      }else {
+        console.log("Success: E-mail cadastrado.")
+        alertaSuccess("Success: E-mail cadastrado.")
       }
-    }
-  })
+    })
+  }else {
+    alertaError("E-mail Inválido")
+  }
 }
 
